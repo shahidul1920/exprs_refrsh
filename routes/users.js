92707e0497dcc5c01220d22b4b17138a96176a20
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-06-22 00:03:36 +0600

    user schema

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+// module.exports = router;
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.1:27017/GenByAuto")
+
+mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  createdAt: { type: Date, default: Date.now }
+})
